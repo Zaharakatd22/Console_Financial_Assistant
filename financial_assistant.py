fe7696797714b856a75b7dd958ee0efc719bcd79
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-09 20:36:20 +0400

    Add functional of consulation and get information about installments credit

--- financial_assistant.py
+++ financial_assistant.py
@@ -1,15 +1,32 @@
-def main():
-    credit_principal = 'Credit principal: 1000'
-    final_output = 'The credit has been repaid!'
-    first_month = 'Month 1: paid out 250'
-    second_month = 'Month 2: paid out 250'
-    third_month = 'Month 3: paid out 500'
+from math import ceil
 
-    print(credit_principal)
-    print(first_month)
-    print(second_month)
-    print(third_month)
-    print(final_output)
+
+def main():
+    print("Enter the credit principal:")
+    credit_principal: int = int(input("> "))
+
+    print("What do you want to calculate?")
+    print('type "m" - for count of months,')
+    print('type "p" - for monthly payment:')
+    request: str = input("> ")
+
+    if request == "m":
+        print("Enter monthly payment:")
+        monthly_payment: int = int(input("> "))
+        months_count: int = ceil(credit_principal / monthly_payment)
+        print()  # decorate separating empty line
+        print(f"It takes {months_count} month to repay the credit" if months_count < 2
+              else f"It takes {months_count} months to repay the credit")
+    elif request == "p":
+        print("Enter count of months:")
+        months_count: int = int(input("> "))
+        monthly_payment: int = ceil(credit_principal / months_count)
+        last_month_payment: int = credit_principal - (months_count - 1) * monthly_payment
+        print()
+        print(f"Your monthly payment = {monthly_payment}" if last_month_payment == monthly_payment
+              else f"Your monthly payment = {monthly_payment} with last month payment = {last_month_payment}.")
+    else:
+        print("Please input correct request.")
 
 
 if __name__ == '__main__':
