cc28e380574d54cf104cdcc8b78e5cf33b42e62a
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-14 14:02:20 +0400

    Add checking on incorrect input.

--- financial_assistant.py
+++ financial_assistant.py
@@ -4,6 +4,30 @@ from math import log
 import argparse
 
 
+def args_len(arguments) -> int:
+    return len(list(filter(None, arguments)))
+
+
+def check_args_negativity(arguments) -> bool:
+    for elem in arguments:
+        if type(elem) != str:
+            if elem < 0:
+                return False
+    return True
+
+
+def check_args(arguments) -> bool:
+    if args_len(list(filter(None, list(arguments.values())))) == 4\
+            and check_args_negativity(list(filter(None, arguments)))\
+            and arguments["credit_interest"] is not None:
+        if arguments["credit_type"] == "diff":
+            if arguments["credit_payment"] is None:
+                return True
+            return False
+        return True
+    return False
+
+
 def main():
     parser = argparse.ArgumentParser(prog="financial_assistant",
                                      description="Calculate diff and annuity type of credit.",
@@ -23,94 +47,65 @@ def main():
     credit_interest: float = args.interest
     credit_payment: float = args.payment
     credit_overpayment: int = 0
-    i: float = (credit_interest / 100) / 12
 
-    if credit_type == "diff":
-        for m in range(1, credit_periods + 1):
-            credit_payment: int = ceil(credit_principal / credit_periods
-                                       + i * (credit_principal
-                                              - (credit_principal * (m - 1) / credit_periods)))
-            credit_overpayment += credit_payment
-            print(f"Month {m}: paid out {credit_payment}")
-
-        credit_overpayment -= credit_principal
-        print(f"Overpayment = {credit_overpayment}")
-
-    elif credit_type == "annuity":
-        if credit_payment is None:
-            credit_payment: int = ceil(credit_principal * (i * (1 + i) ** credit_periods)
-                                       / ((1 + i) ** credit_periods - 1))
-            print(f"Your annuity payment = {credit_payment}!")
-        elif credit_principal is None:
-            credit_principal: int = floor(credit_payment * ((1 + i) ** credit_periods - 1)
-                                          / (i * (1 + i) ** credit_periods))
-            print(f"Your credit principal = {credit_principal}!")
-        else:  # credit_periods is None
-            credit_periods: int = ceil(log(credit_payment / (credit_payment - i * credit_principal), 1 + i))
-            years_count = credit_periods // 12
-            months_count = credit_periods % 12
-            if years_count > 0 and months_count > 0:
-                if years_count == 1 and months_count > 1:
-                    print(f"You need {years_count} year and {months_count} months to repay this credit!")
-                elif years_count > 1 and months_count == 1:
-                    print(f"You need {years_count} years and {months_count} month to repay this credit!")
-                else:
-                    print(f"You need {years_count} years and {months_count} months to repay this credit!")
-            else:
-                if years_count == 0 and months_count > 0:
-                    if months_count > 1:
-                        print(f"You need {months_count} months to repay this credit!")
-                    else:
-                        print(f"You need {months_count} month to repay this credit!")
-                elif years_count > 0 and months_count == 0:
-                    if years_count > 1:
-                        print(f"You need {years_count} years to repay this credit!")
-                    else:
-                        print(f"You need {years_count} year to repay this credit!")
-                else:
-                    print(f"You was already repaid this credit!")
-
-        credit_overpayment = abs(int(credit_payment * credit_periods - credit_principal))
-        print(f"Overpayment = {credit_overpayment}")
+    if not check_args({
+        "credit_type": credit_type,
+        "credit_principal": credit_principal,
+        "credit_periods": credit_periods,
+        "credit_interest": credit_interest,
+        "credit_payment": credit_payment
+    }):
+        print("Incorrect parameters")
     else:
-        print("Incorrect parameters of credit type")
+        i: float = (credit_interest / 100) / 12
 
-    # print("What do you want to calculate?")
-    # print('type "n" - for count of months,')
-    # print('type "a" - for annuity monthly payment,')
-    # print('type "p" - for monthly payment:')
-    # request: str = input("> ")
-    #
-    # if request == "n":
-    #     print("Enter credit principal:")
-    #     credit_principal: int = int(input("> "))
-    #     print("Enter monthly payment:")
-    #     monthly_payment: int = int(input("> "))
-    #     print("Enter credit interest:")
-    #     credit_interest: float = float(input("> "))
-    #     i = (credit_interest / 100) / 12
-    # elif request == "a":
-    #     print("Enter credit principal:")
-    #     credit_principal: int = int(input("> "))
-    #     print("Enter count of periods:")
-    #     periods_count: int = int(input("> "))
-    #     print("Enter credit interest:")
-    #     credit_interest: float = float(input("> "))
-    #     i = (credit_interest / 100) / 12
-    #     annuity_payment: int = ceil(credit_principal * (i * (1 + i) ** periods_count) / ((1 + i) ** periods_count - 1))
-    #
-    # elif request == "p":
-    #     print("Enter monthly payment:")
-    #     monthly_payment: float = float(input("> "))
-    #     print("Enter count of periods:")
-    #     periods_count: int = int(input("> "))
-    #     print("Enter credit interest:")
-    #     credit_interest: float = float(input("> "))
-    #     i = (credit_interest / 100) / 12
-    #     credit_principal: int = round(monthly_payment * ((1 + i) ** periods_count - 1) / (i * (1 + i) ** periods_count))
-    #     print(f"Your credit principal = {credit_principal}!")
-    # else:  # incorrect input
-    #     print("Please input correct request.")
+        if credit_type == "diff":
+            for m in range(1, credit_periods + 1):
+                credit_payment: int = ceil(credit_principal / credit_periods
+                                           + i * (credit_principal
+                                                  - (credit_principal * (m - 1) / credit_periods)))
+                credit_overpayment += credit_payment
+                print(f"Month {m}: paid out {credit_payment}")
+
+            credit_overpayment -= credit_principal
+            print(f"Overpayment = {credit_overpayment}")
+
+        else:  # credit_type == "annuity":
+            if credit_payment is None:
+                credit_payment: int = ceil(credit_principal * (i * (1 + i) ** credit_periods)
+                                           / ((1 + i) ** credit_periods - 1))
+                print(f"Your annuity payment = {credit_payment}!")
+            elif credit_principal is None:
+                credit_principal: int = floor(credit_payment * ((1 + i) ** credit_periods - 1)
+                                              / (i * (1 + i) ** credit_periods))
+                print(f"Your credit principal = {credit_principal}!")
+            else:  # credit_periods is None
+                credit_periods: int = ceil(log(credit_payment / (credit_payment - i * credit_principal), 1 + i))
+                years_count = credit_periods // 12
+                months_count = credit_periods % 12
+                if years_count > 0 and months_count > 0:
+                    if years_count == 1 and months_count > 1:
+                        print(f"You need {years_count} year and {months_count} months to repay this credit!")
+                    elif years_count > 1 and months_count == 1:
+                        print(f"You need {years_count} years and {months_count} month to repay this credit!")
+                    else:
+                        print(f"You need {years_count} years and {months_count} months to repay this credit!")
+                else:
+                    if years_count == 0 and months_count > 0:
+                        if months_count > 1:
+                            print(f"You need {months_count} months to repay this credit!")
+                        else:
+                            print(f"You need {months_count} month to repay this credit!")
+                    elif years_count > 0 and months_count == 0:
+                        if years_count > 1:
+                            print(f"You need {years_count} years to repay this credit!")
+                        else:
+                            print(f"You need {years_count} year to repay this credit!")
+                    else:
+                        print(f"You was already repaid this credit!")
+
+            credit_overpayment = abs(int(credit_payment * credit_periods - credit_principal))
+            print(f"Overpayment = {credit_overpayment}")
 
 
 if __name__ == '__main__':
